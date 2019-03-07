03fbc9278c2fbb42d2298b5dabc717b9cb243641
Durian <karen.kua7@gmail.com>
2019-03-06 22:30:34 -0500

    Added sign in and sign out methods for google OAuth

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react"
 
 class GoogleAuth extends Component {
+    state = {
+        isSignedIn: null
+    }
 
     componentDidMount = () => {
         window.gapi.load("client:auth2", () => {
@@ -8,11 +11,31 @@ class GoogleAuth extends Component {
                 clientId: "159628228181-fdl60fq592tl2cbj234k51e40qis2lpg.apps.googleusercontent.com",
                 scope: "email"
             })
+            .then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance()
+                this.setState({isSignedIn: this.auth.isSignedIn.get()})
+                this.auth.isSignedIn.listen(this.onAuthChange)
+            })
         })
     }
+
+    onAuthChange = () => {
+        this.setState({isSignedIn: this.auth.isSignedIn.get()})
+    }
+    
+    renderAuthButton = () => {
+        if (this.state.isSignedIn === null) {
+            return <div>I don't know if we are signed in</div>
+        } else if (this.state.isSignedIn) {
+            return<div>I am signed in</div>
+        } else {
+            return <div>I am not signed in</div>
+        }
+    }
+
     render() {
         return(
-            <div>Google Auth</div>
+            <div>{this.renderAuthButton()}</div>
         )
     }
 }
